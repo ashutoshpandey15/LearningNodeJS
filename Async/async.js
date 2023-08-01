d33edb969322e4d9f2012730f59ed260cc02a20c
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-08-01 12:16:25 +0530

    Async Core Modules

---
+++ Async/async.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+// Async Method of File System
+
+// To create a text file
+fs.writeFile("ashu.txt", "Hello THis is my First txt Document", (err) => {
+    console.log(err)
+})
+// To add some additonal text to the file
+fs.appendFile("ashu.txt", "  Hello THis is my Second txt Document", (error) => {
+    console.log(error)
+})
+//To Read the txt File 
+fs.readFile("ashu.txt", "utf-8", (error, data) => {
+    console.log(data)
+})
