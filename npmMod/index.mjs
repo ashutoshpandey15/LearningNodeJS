c44e451f27ef0aa4b4ceb4e8db5e7b62ffd1d83c
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-08-04 20:11:56 +0530

    npm

--- npmMod/index.mjs
+++ npmMod/index.mjs
@@ -1,3 +1,5 @@
 import chalk from "chalk";
-
+import validator from "validator";
 console.log(chalk.blue.inverse("Hello world!"));
+const res = validator.isEmail("foo@bar.com");
+console.log(res ? chalk.red.inverse(res) : chalk.green.inverse(res));
