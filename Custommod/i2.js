41fd8666ec3cd77233d3a6e5ab2b8f371e4d732f
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-08-05 13:29:41 +0530

    custom modules

---
+++ Custommod/i2.js
@@ -0,0 +1,5 @@
+const { add, sub, mul } = require("./i1"); // importing and using them
+
+console.log(add(5, 10));
+console.log(sub(5, 10));
+console.log(mul(5, 10));
--- npmMod/index.mjs
+++ npmMod/index.mjs
@@ -1,5 +1,5 @@
 import chalk from "chalk";
 import validator from "validator";
 console.log(chalk.blue.inverse("Hello world!"));
-const res = validator.isEmail("foo@bar.com");
-console.log(res ? chalk.red.inverse(res) : chalk.green.inverse(res));
+const res = validator.isEmail("foo@bar.m");
+console.log(res ? chalk.white.inverse(res) : chalk.blue.inverse(res));
