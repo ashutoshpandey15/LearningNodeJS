ae725713f571c9815b040103a92632287a44aa56
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-08-07 19:40:50 +0530

    routing done

--- webserver/id.js
+++ webserver/id.js
@@ -1,6 +1,14 @@
-const http = require("http");
+const http = require("http"); //importing http module
+const url = require("url"); // importing url module
 const server = http.createServer((req, res) => {
-  res.end("Hello Form this side of the page");
+  if (req.url == "/") {
+    res.end("Hello From this side of the page");
+  } else if (req.url == "/about") {
+    res.end("Hello From about side of the page");
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" }); // 404 is the status code and the second parameter is the object
+    res.end("<h1>404 error page. Page does not exist</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
