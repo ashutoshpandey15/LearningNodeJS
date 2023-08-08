91ac05a1a04f67ca33fcf16779dacf8ac3d3236f
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-08-08 17:31:33 +0530

    Till events

--- eventmodule/index2.js
+++ eventmodule/index2.js
@@ -11,3 +11,9 @@ event.emit("sayMyName"); // creating a custom event
 // The basic logic is that we create an event emitter and then we listen
 // to that event emitter and then we emit that event emitter
 //First Listennig to the event and then emitting the event
+
+event.on("checkPage", (sc, msg) => {
+  console.log(`status code is ${sc} and the page is ${msg}`);
+});
+
+event.emit("checkPage", 200, "ok"); // emitting the event with status code 200 and mssg
