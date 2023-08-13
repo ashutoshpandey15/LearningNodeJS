6a465374e463c5c42ed995c789c3b61c64f86ab4
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-08-14 02:28:22 +0530

    Update products.js

--- LearningExpress/API/controllers/products.js
+++ LearningExpress/API/controllers/products.js
@@ -4,13 +4,15 @@
 const Product = require("../models/product");
 
 const getAllProducts =async (req, res) => {
-    const myData =await Product.find({})  
+    const myData =await Product.find(req.query)  
      //find fxn will find all the data from the database
     res.status(200).json({myData})
 }
 //status 200 means ok dedo and ye mssg pass kardo json me agar ho gaya toh
 const getAllProductsTesting =async (req, res) => {
-res.status(200).json({msg:"Iam getall productstesting"})
+    const myData =await Product.find(req.query)  
+    //find fxn will find all the data from the database
+   res.status(200).json({myData})
 }
 
 module.exports = {getAllProducts,getAllProductsTesting}
