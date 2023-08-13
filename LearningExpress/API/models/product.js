23c315c3bdbf5f21d30d03400717af595ff85e11
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-08-14 01:07:22 +0530

    Create product.js

---
+++ LearningExpress/API/models/product.js
@@ -0,0 +1,35 @@
+// Defining the schema and model for Product
+
+const mongoose = require('mongoose');
+
+constproductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'product name must be provided'],
+
+    },
+    price: {
+        type: Number,
+        required: [true, 'product price must be provided'],
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+
+    },
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    company: {
+        type: String,
+        enum:{
+            values:['apple','samsung','dell','mi','lg','google','amazon','microsoft','ibm','tesla','facebook','twitter','instagram','youtube','tiktok','snapchat','whatsapp','telegram','signal','discord','zoom','skype','linkedin','netflix','spotify','twitch','github','stackoverflow','quora','reddit','pinterest','tumblr','medium','wordpress','blogger','wix','weebly',''],
+            message:`{VALUE} is not supported`
+        },
+    },
+})
