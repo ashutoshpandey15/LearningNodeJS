966f3f992cf3f4cab595b42f0b1a8dffd7357981
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-08-13 23:35:58 +0530

    Update products.js

--- LearningExpress/API/routes/products.js
+++ LearningExpress/API/routes/products.js
@@ -1,10 +1,16 @@
 // routes basically will tell us about where we want to go / route 
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 
+
+// yahan pe hum jo route ke liye fxn define kiye the uko require kar rahe
 const {getAllProducts,getAllProductsTesting}= require("../controllers/products")
 
-router.route("/").get(getAllProducts);          //getAllProducts is a Fxn 
+// ******************************************************
+//router.route("kahan karna hai").get("kaise karna hai")
+// ******************************************************
+//getAllProducts is a Fxn 
+router.route("/").get(getAllProducts);          
 router.route("/testing").get(getAllProductsTesting);
 
 
