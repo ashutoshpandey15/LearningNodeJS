c565f2f8c9a4685f7c01cee394b9370b71395051
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-08-13 23:45:54 +0530

    Update ashuapi.js

--- LearningExpress/API/ashuapi.js
+++ LearningExpress/API/ashuapi.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express();
 
 
-
-const product_routes =require("./routes/products")
+//jinko bhi route kiya tha unko idhar require kar rahe hai
+const product_routes =require("./routes/products");
 app.get("/",(req,res) => {
     res.send("Hi iam live ")
 });
 
 //middleware to set router
+// The app.use() function is used to mount the specified
+// middleware function(s) at the path which is being specified. It is mostly 
+// used to set up middleware for your application.
+// basically hum app.use se middleware ko ek particular path pe set kar rahe hai
+// app.use("humko basically localhost:8000/ ke baad me kya chaiye",const jisme routes save kiya tha)
 app.use("/api/products",product_routes)
 
 
